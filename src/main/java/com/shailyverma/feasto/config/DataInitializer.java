e5920b6fb40b914337fe9db7b3ad93bd39b96085
Shaily Verma <vermashaily492@gmail.com>
2026-05-25 13:59:38 +0530

    added automatic role initializer

---
+++ src/main/java/com/shailyverma/feasto/config/DataInitializer.java
@@ -0,0 +1,35 @@
+package com.shailyverma.feasto.config;
+
+import com.shailyverma.feasto.auth_users.entity.Role;
+import com.shailyverma.feasto.auth_users.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) {
+
+        createRoleIfNotExists("ADMIN");
+        createRoleIfNotExists("CUSTOMER");
+        createRoleIfNotExists("DELIVERY");
+    }
+
+    private void createRoleIfNotExists(String roleName) {
+
+        if (roleRepository.findByName(roleName).isEmpty()) {
+
+            Role role = new Role();
+            role.setName(roleName);
+
+            roleRepository.save(role);
+
+            System.out.println(roleName + " role created.");
+        }
+    }
+}
